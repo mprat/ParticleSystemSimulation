73a8f61b8b989250c59aae21b43896288fbad91b
mprat <michele.pratusevich@gmail.com>
2012-11-01 00:12:24 -0400

    sphere not being drawn in the center anymore

--- distrib/ClothSystem.cpp
+++ distrib/ClothSystem.cpp
@@ -135,39 +135,6 @@ vector<Vector3f> ClothSystem::evalF(vector<Vector3f> state)
 	return f;
 }
 
-//
-//// for a given state, evaluate f(X,t)
-//vector<Vector3f> ClothSystem::evalF(vector<Vector3f> state)
-//{
-//	vector<Vector3f> f(state);
-//
-//	//add gravity and drag and velocity
-//	for (int i = 0; i < m_numParticles; i++)
-//	{
-//		f[2*i] = velocityOf(i, state);
-//		f[2*i + 1] = Vector3f(0, 0, 0);
-//		f[2*i + 1] += g.getForce(mass[i]);
-//		f[2*i + 1] += d.getForce(velocityOf(i, state));
-//	}
-//	for (unsigned i = 0; i < springs.size();  i++)
-//	{
-//		f[2*(springs[i].i) + 1] += springs[i].getForce(positionOf(springs[i].i, state), positionOf(springs[i].j, state));
-//		f[2*(springs[i].j) + 1] += springs[i].getForce(positionOf(springs[i].j, state), positionOf(springs[i].i, state));
-//	}
-//
-//	//normalize by mass
-//	for (int i = 0; i < m_numParticles; i++){
-//		f[2*i + 1] = f[2*i + 1] / mass[i];
-//	}
-//	
-//	//set fixed points
-//	for (unsigned i = 0; i < fixedpoints.size(); i++){
-//		f[2*fixedpoints[i] + 1] = Vector3f(0, 0, 0);
-//	}
-//	
-//	return f;
-//}
-
 //cloth is initialized like
 //0 --- 1 --- 2 --- 3
 //4 --- 5 --- 6 --- 7...etc.
--- distrib/main.cpp
+++ distrib/main.cpp
@@ -92,10 +92,7 @@ namespace
     
     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, particleColor);
     
-    glutSolidSphere(0.1f,10.0f,10.0f);
-    
     system->draw();
-    
     
     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, floorColor);
     glPushMatrix();
